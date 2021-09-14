fb30b081869ead8baa8b95f9c917d2535be3e684
hsjeong <nicewook@hotmail.com>
2021-09-14 16:18:44 +0900

    feat: add test to basic api example

--- basic-api-1/main.go
+++ basic-api-1/main.go
@@ -7,37 +7,48 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type TestModel struct {
+type Account struct {
 	Id   int    `json:"id" binding:"required`
 	Name string `json:"name" binding:"required`
 }
 
-func main() {
-	r := gin.Default()
-	r.GET("", func(c *gin.Context) {
-		// c.String(http.StatusOK, "hello world!")
-		c.JSON(http.StatusOK, gin.H{
-			"responseData": "hello JSON wonrld",
-		})
+func helloHandler(c *gin.Context) {
+	// c.String(http.StatusOK, "hello world!")
+	c.JSON(http.StatusOK, gin.H{
+		"responseData": "hello world",
 	})
-	r.GET("/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		c.JSON(http.StatusOK, gin.H{
-			"greetings": fmt.Sprintf("hello %v", name),
-		})
-	})
-	r.POST("/add", func(c *gin.Context) {
-		var data TestModel
-		if err := c.ShouldBind(&data); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": fmt.Sprintf("err: %v", err),
-			})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"dataReceived": data,
-		})
-	})
+}
 
-	r.Run("localhost:8080")
+func helloAccountHandler(c *gin.Context) {
+	var data Account
+	if err := c.ShouldBind(&data); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": fmt.Sprintf("err: %v", err),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"dataReceived": data,
+	})
+}
+
+func helloUserHandler(c *gin.Context) {
+	name := c.Param("name")
+	c.JSON(http.StatusOK, gin.H{
+		"greetings": fmt.Sprintf("hello %v", name),
+	})
+}
+
+func main() {
+	newServer().Run()
+}
+
+func newServer() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("", helloHandler)
+	r.GET("/:name", helloUserHandler)
+	r.POST("/add", helloAccountHandler)
+
+	return r
 }
---
+++ basic-api-1/main_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestBasicAPI(t *testing.T) {
+
+	ts := httptest.NewServer(newServer())
+	defer ts.Close()
+
+	printResponseBody := func(resp *http.Response) {
+		body, err := ioutil.ReadAll(resp.Body)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		t.Log(string(body))
+	}
+
+	t.Run("hello GET", func(t *testing.T) {
+		t.Log(ts.URL)
+		resp, err := http.Get(fmt.Sprintf("%s", ts.URL))
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != 200 {
+			t.Fatalf("Expected status code 200, got %v", resp.StatusCode)
+		}
+		printResponseBody(resp)
+	})
+	t.Run("hello name GET", func(t *testing.T) {
+		t.Log(ts.URL)
+		resp, err := http.Get(fmt.Sprintf("%s/hsjeong", ts.URL))
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != 200 {
+			t.Fatalf("Expected status code 200, got %v", resp.StatusCode)
+		}
+		printResponseBody(resp)
+	})
+	t.Run("json POST", func(t *testing.T) {
+		t.Log(ts.URL)
+		account := Account{10, "Alex"}
+		b, _ := json.Marshal(account)
+		buff := bytes.NewBuffer(b)
+		resp, err := http.Post(fmt.Sprintf("%s/add", ts.URL), "application/json", buff)
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != 200 {
+			t.Fatalf("Expected status code 200, got %v", resp.StatusCode)
+		}
+		printResponseBody(resp)
+	})
+}
