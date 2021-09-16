73f16e1c61c34cf8e82e76a0f469acfe3a02f691
root <root@JHS-IRISID.localdomain>
2021-09-16 19:33:00 +0900

    example2: bind-data-2

--- bind-data-2/main_test.go
+++ bind-data-2/main_test.go
@@ -21,11 +21,11 @@ func TestBindDataAPI(t *testing.T) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		t.Log(string(body))
+		t.Logf("StatusCode: %v, responseBody: %v", resp.StatusCode, string(body))
 	}
 
 	t.Run("GET Query BadReqeust", func(t *testing.T) {
-		resp, err := http.Get(fmt.Sprintf("%s/user?id=1&password=qwer", ts.URL))
+		resp, err := http.Get(fmt.Sprintf("%s/user?id=a&password=qwer", ts.URL))
 		if err != nil {
 			t.Fatalf("Expected no error, got %v", err)
 		}
@@ -64,9 +64,10 @@ func TestBindDataAPI(t *testing.T) {
 		}
 		defer resp.Body.Close()
 
-		if resp.StatusCode != http.StatusBadRequest {
-			t.Logf("Expected status code %v, got %v", http.StatusBadRequest, resp.StatusCode)
+		if resp.StatusCode != http.StatusOK {
+			t.Fatalf("Expected status code %v, got %v. even POST wrong JSON. it does not matter", http.StatusOK, resp.StatusCode)
 		}
+		t.Logf("even POST wrong JSON. it does not matter. StatusCode is %v", resp.StatusCode)
 		printResponseBody(resp)
 	})
 
@@ -85,7 +86,7 @@ func TestBindDataAPI(t *testing.T) {
 		defer resp.Body.Close()
 
 		if resp.StatusCode != http.StatusOK {
-			t.Logf("Expected status code %v, got %v", http.StatusOK, resp.StatusCode)
+			t.Fatalf("Expected status code %v, got %v", http.StatusOK, resp.StatusCode)
 		}
 		printResponseBody(resp)
 	})
@@ -99,8 +100,8 @@ func TestBindDataAPI(t *testing.T) {
 		}
 		defer resp.Body.Close()
 
-		if resp.StatusCode != http.StatusBadRequest {
-			t.Logf("Expected status code %v, got %v", http.StatusBadRequest, resp.StatusCode)
+		if resp.StatusCode != http.StatusNotFound {
+			t.Fatalf("Expected status code %v, got %v", http.StatusNotFound, resp.StatusCode)
 		}
 		printResponseBody(resp)
 	})
@@ -115,7 +116,7 @@ func TestBindDataAPI(t *testing.T) {
 		defer resp.Body.Close()
 
 		if resp.StatusCode != http.StatusOK {
-			t.Logf("Expected status code %v, got %v", http.StatusOK, resp.StatusCode)
+			t.Fatalf("Expected status code %v, got %v", http.StatusOK, resp.StatusCode)
 		}
 		printResponseBody(resp)
 	})
@@ -130,20 +131,21 @@ func TestBindDataAPI(t *testing.T) {
 		defer resp.Body.Close()
 
 		if resp.StatusCode != http.StatusBadRequest {
-			t.Logf("Expected status code %v, got %v", http.StatusBadRequest, resp.StatusCode)
+			t.Fatalf("Expected status code %v, got %v", http.StatusBadRequest, resp.StatusCode)
 		}
 		printResponseBody(resp)
 	})
 
 	t.Run("PUT URI JSON OK", func(t *testing.T) {
 		account := User{
+			ID:    3,
 			Name:  "Hyunseok, Jeong",
 			Email: "a@gmail.com",
 		}
 		b, _ := json.Marshal(account)
 		buff := bytes.NewBuffer(b)
 		client := &http.Client{}
-		req, _ := http.NewRequest(http.MethodPut, fmt.Sprintf("%s/user/1/wrongName", ts.URL), buff)
+		req, _ := http.NewRequest(http.MethodPut, fmt.Sprintf("%s/user/1", ts.URL), buff)
 		resp, err := client.Do(req)
 		if err != nil {
 			t.Fatalf("Expected no error, got %v", err)
@@ -151,7 +153,7 @@ func TestBindDataAPI(t *testing.T) {
 		defer resp.Body.Close()
 
 		if resp.StatusCode != http.StatusOK {
-			t.Logf("Expected status code %v, got %v", http.StatusOK, resp.StatusCode)
+			t.Fatalf("Expected status code %v, got %v", http.StatusOK, resp.StatusCode)
 		}
 		printResponseBody(resp)
 	})
